0d6926d8073435f992416a514992d8e0036ede9c
tom-schier <schiertxs@gmail.com>
2022-02-27 16:48:32 +1100

    Added DSB disclaimer

--- dist/app.js
+++ dist/app.js
@@ -24,7 +24,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var dsb = __importStar(require("dsb-middleware"));
-var app = express_1.default();
+var app = (0, express_1.default)();
 var port = 3000;
 app.use(dsb.dsbHeaders);
 app.get('/', function (req, res, next) {
