1835fa383bf941b9e78ba2a8e11a17227fca72e3
Tomas Schier <tomas@xpsolutions.com.au>
2022-08-16 17:17:28 +1000

    Additional comments in app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,29 +8,36 @@ import bodyParser from 'body-parser';
 const exp = express;
 const app = express();
 const port = 3000;
+let standardsVersion = '/cds-au/v1';
 
 // the endpoint configuration file fort this server
 const dsbEndpoints = [...endpoints] as EndpointConfig[];
 
-
+// Used in cdrJwtScopes. This configuration is specific to the DSB mock data holder
+// ie access token is JWT and scope in that token is a space delimited string
 const authOptions: DsbAuthConfig = {
     scopeFormat: 'STRING',
     endpoints: dsbEndpoints
 }
 
+// Used cdrAuthorisation and cdrHeaders. 
 const dsbOptions: CdrConfig = {
     endpoints: dsbEndpoints
 }
 
-//app.use(exp.json());
-//app.use(cdrJwtScopesListSeparated);
-app.use(bodyParser);
+// This middle ware will extend the request object with the scopes.
+// It can be used for any IDAM where the access token is a JWT and the 
+// scope property is either an array of string or a space seperated string
 app.use(cdrJwtScopes(authOptions));
+
+// This middle ware will check access tokens for existence and scope
 app.use(cdrAuthorisation(dsbOptions));
+
+// this middle ware will handle the boilerplate validation and setting for a number of header parameters
 app.use(cdrHeaders(dsbOptions));
 
 
-let standardsVersion = '/cds-au/v1';
+
 
 // this endpoint does NOT reequire authentication
 app.get(`${standardsVersion}/energy/plans`, (req: Request, res: Response, next: NextFunction) => {
