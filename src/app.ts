a56f7ddfbc380b62cfeb0d3fa91673b80a70cb33
Tomas Schier <tomas@xpsolutions.com.au>
2024-01-20 18:09:24 +1100

    Use of new holder-sdk, some refactor

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express, { request }  from 'express';
 import {NextFunction, Request, Response} from 'express';
 import endpoints from './data/endpoints.json';
-import { EndpointConfig, CdrConfig, cdrScopeValidator, cdrHeaderValidator, cdrResourceValidator, cdrEndpointValidator, IUserService}  from '@cds-au/holder-sdk'
+import { EndpointConfig, CdrConfig, cdrScopeValidator, cdrHeaderValidator, cdrResourceValidator, cdrEndpointValidator, IUserService, cdrTokenValidator, cdrJwtScopes, DsbAuthConfig}  from '@cds-au/holder-sdk'
 import { CdrUser } from '@cds-au/holder-sdk/src/models/user';
-
+import http from 'http'
+import https from 'https'
 
 const app = express();
 const port = 3000;
@@ -20,9 +21,22 @@ let standardsVersion = '/cds-au/v1';
 const sampleEndpoints = [...endpoints] as EndpointConfig[];
 
 
-// Used in the and cdrEndpointValidator cdrHeaderValidator functions. 
-const dsbOptions: CdrConfig = {
+// Used in the and cdrEndpointValidatorfunctions
+const endpointConfig: CdrConfig = {
+    endpoints: sampleEndpoints,
+    // basePath: '/prod-data'
+    //specifiedEndpointsOnly: false
+}
+
+// Used in the cdrHeaderValidator function
+const headerConfig: CdrConfig = {
     endpoints: sampleEndpoints
+}
+
+// Used in the cdrJwtScopes function
+const jwtConfig: DsbAuthConfig = {
+    endpoints: sampleEndpoints,
+    scopeFormat: 'LIST'
 }
 
 // Some middleware requires a IUserService, which will be called at runtime to get the current user
@@ -34,18 +48,23 @@ const userService: IUserService = {
     getUser(): CdrUser {
         let usr : CdrUser = {
             accountsEnergy:['12345'],
-            accountsBanking:['234324'],
+            accountsBanking:['6789'],
             energyServicePoints: ['34563'],
             scopes_supported: [
                 'energy:accounts.basic:read',
-                'bank:payees:read',
                 'energy:accounts.detail:read',
+                'bank:payees:read',
                 'energy:electricity.servicepoints.basic:read',
                 'energy:electricity.servicepoints.detail:read',
                 'bank:accounts.basic:read']
         }
         return usr;
     }
+}
+
+// Used in the cdrTokenValidator function
+const tokenConfig: CdrConfig = {
+    endpoints: sampleEndpoints,
 }
 
 // function used to determine if the middleware is to be bypassed for the given 'paths'
@@ -56,17 +75,29 @@ function unless(middleware: any, ...paths: any) {
     };
 };
 
-// This function will check if this is a CDR endpoint, and return appropriate error if it is not
-app.use(unless(cdrEndpointValidator(dsbOptions), "/energy/plans"));
-// This function will validate the accounts of the user in the request url
-app.use(cdrResourceValidator(userService));
-// This function will validate the scope assigned to the user against scope required
-app.use(cdrScopeValidator(userService));
-// this function will handle the boilerplate validation and setting for a number of header parameters
-app.use(cdrHeaderValidator(dsbOptions));
+// This function will check if this is a CDR endpoint and if it has been implemented
+app.use(cdrEndpointValidator(endpointConfig));
+// This function will validate various headers required under the CDR
+app.use(cdrHeaderValidator(headerConfig));
 
-// this endpoint is not a CDR endpoint
-app.get(`/energy/plans`, (req: Request, res: Response, next: NextFunction) => {
+app.use(cdrScopeValidator(userService));
+// **** An alternative method to valid scop is to use cdrJwtScopes in combination with cdrTokenValidator
+// app.use(cdrJwtScopes(jwtConfig))
+// app.use(cdrTokenValidator(tokenConfig));
+
+// This function will validate consent has been given to specific accounts
+app.use(cdrResourceValidator(userService));
+
+
+// This function will validate the accounts of the user in the request url
+// app.use(cdrResourceValidator(userService));
+// // This function will validate the scope assigned to the user against scope required
+// app.use(cdrScopeValidator(userService));
+// // this function will handle the boilerplate validation and setting for a number of header parameters
+// app.use(cdrHeaderValidator(config));
+
+// this endpoint is not a CDR endpoint, but is marked to by pass the cdrEndpointValidator
+app.get(`/ignore-me`, (req: Request, res: Response, next: NextFunction) => {
     let st = `Received request on ${port} for ${req.url}`;
     console.log(st);
     res.send(st);
@@ -86,69 +117,74 @@ app.get(`${standardsVersion}/energy/accounts`, (req: Request, res: Response, nex
     res.send(st);
 });
 
-// this endpoint requires authentication
-app.get(`${standardsVersion}/energy/electricity/servicepoints`, (req: Request, res: Response, next: NextFunction) => {
-    let st = `Received request on ${port} for ${req.url}`;
-    console.log(st);
-    res.send(st);
-});
-
-// this endpoint requires authentication
-app.get(`${standardsVersion}/energy/electricity/servicepoints/:servicePointId`, (req: Request, res: Response, next: NextFunction) => {
-    let st = `Received request on ${port} for ${req.url}`;
-    console.log(st);
-    res.send(st);
-});
-
-
-// this endpoint requires authentication
-app.get(`${standardsVersion}/energy/electricity/servicepoints/:servicePointId/usage`, (req: Request, res: Response, next: NextFunction) => {
-    let st = `Received request on ${port} for ${req.url}`;
-    console.log(st);
-    res.send(st);
-});
-
-// this endpoint requires authentication
+// this endpoint requires authentication and a consented accountId
 app.get(`${standardsVersion}/energy/accounts/:accountId`, (req: Request, res: Response, next: NextFunction) => {
     let st = `Received request on ${port} for ${req.url}`;
     console.log(st);
     res.send(st);
 });
 
-
-// this endpoint requires authentication
-app.get(`${standardsVersion}/banking/accounts/:accountId/balance`, (req: Request, res: Response, next: NextFunction) => {
-    let st = `Received request on ${port} for ${req.url}`;
-    console.log(st);
-    res.send(st);
-});
-
-
-// this endpoint requires authentication
-app.get(`${standardsVersion}/banking/accounts/:accountId/payments/scheduled`, (req: Request, res: Response, next: NextFunction) => {
+// this endpoint is not a CDR endpoint
+app.get(`/health`, (req: Request, res: Response, next: NextFunction) => {
     let st = `Received request on ${port} for ${req.url}`;
     console.log(st);
     res.send(st);
 });
 
 // this endpoint requires authentication
-app.get(`${standardsVersion}/banking/payments/scheduled`, (req: Request, res: Response, next: NextFunction) => {
+app.get(`${standardsVersion}/banking/accounts/:accountId`, (req: Request, res: Response, next: NextFunction) => {
     let st = `Received request on ${port} for ${req.url}`;
     console.log(st);
     res.send(st);
 });
 
 // this endpoint requires authentication
-app.get(`${standardsVersion}/banking/payees`, (req: Request, res: Response, next: NextFunction) => {
+app.get(`/prod-data${standardsVersion}/banking/accounts`, (req: Request, res: Response, next: NextFunction) => {
     let st = `Received request on ${port} for ${req.url}`;
     console.log(st);
     res.send(st);
 });
+
+
+
+// // this endpoint requires authentication
+// app.get(`${standardsVersion}/banking/accounts/:accountId/balance`, (req: Request, res: Response, next: NextFunction) => {
+//     let st = `Received request on ${port} for ${req.url}`;
+//     console.log(st);
+//     res.send(st);
+// });
+
+
+// // this endpoint requires authentication
+// app.get(`${standardsVersion}/banking/accounts/:accountId/payments/scheduled`, (req: Request, res: Response, next: NextFunction) => {
+//     let st = `Received request on ${port} for ${req.url}`;
+//     console.log(st);
+//     res.send(st);
+// });
+
+// // this endpoint requires authentication
+// app.get(`${standardsVersion}/banking/payments/scheduled`, (req: Request, res: Response, next: NextFunction) => {
+//     let st = `Received request on ${port} for ${req.url}`;
+//     console.log(st);
+//     res.send(st);
+// });
+
+// // this endpoint requires authentication
+// app.get(`${standardsVersion}/banking/payees`, (req: Request, res: Response, next: NextFunction) => {
+//     let st = `Received request on ${port} for ${req.url}`;
+//     console.log(st);
+//     res.send(st);
+// });
 
 app.get('/', (req, res, next) => {
     console.log(`Received request on BASE ${port}`);
     res.send();
 });
+
+
+http.createServer(app).listen(80);
+
+https.createServer(app).listen(443);
 
 
 app.listen(port, hostname, () => {
